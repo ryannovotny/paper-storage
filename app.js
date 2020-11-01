350434c36f384cd30f13612ceb6461409eb52596
Ryan Novotny <ryanov1333@gmail.com>
2020-11-01 11:38:54 -0800

    Add MongoDB connection. Add paths for node_module packages

--- app.js
+++ app.js
@@ -5,8 +5,27 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// Add Mongoose for MongoDB
+var mongoose = require('mongoose').set('debug', true);
+mongoose.Promise = global.Promise;
+
+// Set Mongoose Options
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  }
+
+// Connect to MongoDB Server
+mongoose.connect('mongodb://ryan:7926Sacjdm*7926@192.168.1.135:27017/Papers?authSource=admin', options)
+.then(() => console.log( 'Database Connected!' ))
+.catch(err => console.log( err ));
+
+// Add Mongoose schema
+require('./models/Papers');
+
+// Add Needed Routes
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -17,12 +36,16 @@ app.set('view engine', 'ejs');
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use('/popper', express.static(__dirname + '/node_modules/popper.js/dist'));
+app.use('/fontawesome', express.static(__dirname + '/node_modules/@fortawesome/fontawesome-free'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/bs-table', express.static(__dirname + '/node_modules/bootstrap-table/dist'));
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
