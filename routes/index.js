211159e4a749a71c9ac1a5fd6b8e1bdaa1c0022f
Ryan Novotny <ryanov1333@gmail.com>
2020-11-01 11:42:00 -0800

    Add route for views and querying

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var paper = require("../controllers/PaperController.js");
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/layout', function(req, res) {
-  res.render('layout', { title: 'Express' });
+router.get('/add', function(req, res) {
+  res.render('add', { title: 'Add Paper' });
 });
 
+router.get('/edit', function(req, res) {
+  res.render('edit', { title: 'Edit Paper Entry' });
+});
+
+router.get('/remove', function(req, res) {
+  res.render('remove', { title: 'Remove Paper' });
+});
+
+// Get all items
+router.get('/list', paper.list);
 
 module.exports = router;
