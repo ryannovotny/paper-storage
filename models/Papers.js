4fa0bc26b637c8609d8670699c846aa6923a8e3a
Ryan Novotny <ryanov1333@gmail.com>
2020-11-24 01:56:21 -0800

    Change authors to array. Add publish date and tags

--- models/Papers.js
+++ models/Papers.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 
 var PapersSchema = new mongoose.Schema({
   title: {type: String},
-  authors: {type: String},
+  authors: [{type: String}],
   journal: {type: String},
-  topic: {type: String},
   doi: {type: String},
-  added: {type: Date}
+  published: {type: Date},
+  tags: [{type: String}]
 });
 
 module.exports = mongoose.model('Papers', PapersSchema);
