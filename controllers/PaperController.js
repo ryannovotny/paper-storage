75515109661bbf557295e17a43730e90451584b4
Ryan Novotny <ryanov1333@gmail.com>
2020-11-24 01:55:49 -0800

    Update search controller for new model

--- controllers/PaperController.js
+++ controllers/PaperController.js
@@ -6,18 +6,37 @@ var PapersController = {};
 
 // Show list of all items
 PapersController.list = function(req, res) {
-    Papers.find({"title": {$exists: true}},
-      function (err, papers) {
-        if (err) {
-            console.log(err);
-            res.sendStatus(500);
-        }
-        else {
-            console.log(papers)
-            res.send(papers);
-        }
+  Papers.find({"title": {$exists: true}},
+    function (err, papers) {
+      if (err) {
+          console.log(err);
+          res.sendStatus(500);
+      } else {
+          console.log(papers)
+          res.send(papers);
       }
-    );
+    });
   };
+
+// Search for papers
+PapersController.search = function(req, res) {
+  var regexEx = new RegExp(req.query.srch, "i");
+  Papers.find(
+    { $or: [ {"title": regexEx},
+             {"authors": regexEx},
+             {"journal": regexEx},
+             {"tags": regexEx} ] },
+    function (err, papers) {
+      if (err) {
+        console.log("Search Error: " + err);
+        res.sendStatus(500);
+      }
+      else {
+        console.log(papers);
+        res.send(papers);
+      }
+    }
+  );
+};
 
   module.exports = PapersController;
